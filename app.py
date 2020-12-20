8f2301cb5b791507a85eb478a96d8f95d8be4988
Prashant Nair <prashant.nair2050@gmail.com>
2020-12-20 15:47:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pickle
 
 stopwordsList = pickle.load(open('stopwords.list', 'rb'))
 
+
 def textPreprocessor(featureRecord):
     #a.Remove Punctuation
     removePunctuation = [char for char in featureRecord if char not in string.punctuation]
@@ -66,4 +67,20 @@ def predict():
     return render_template('index.html', prediction_text=' The SMS entered is a {}'.format(predLabel))
 
 if __name__ == "__main__":
+    def textPreprocessor(featureRecord):
+    #a.Remove Punctuation
+    removePunctuation = [char for char in featureRecord if char not in string.punctuation]
+    sentences = ''.join(removePunctuation)
+    
+    #b.Convert Sentences to Words
+    words = sentences.split(" ")
+    
+    #c. Normalize
+    wordNormalized = [word.lower() for word in words]
+    
+    #d. Remove Stopwords
+    finalWords = [word for word in wordNormalized if word not in stopwordsList]
+    
+    return finalWords
+
     app.run(debug=True)
